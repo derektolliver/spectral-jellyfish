c524651970c950d7a9079c4577ef56cf8ffc9723
Derek <derekt1026@gmail.com>
2016-08-17 21:58:58 -0500

    Palindrome Number

---
+++ Leetcode/PalindromeNumber.java
@@ -0,0 +1,14 @@
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        } else {
+            int rev = 0;
+            while (x > rev) {
+                rev = rev * 10 + x % 10;
+                x /= 10;
+            }
+            return x == rev || x == rev % 10;
+        }
+    }
+}
