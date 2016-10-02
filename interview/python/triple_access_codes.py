6ec495187f8c413105951951f5a0506c83c2de41
Derek <derekt1026@gmail.com>
2016-10-02 18:43:10 -0500

    Triple Access Codes, I made this way harder than it needed to be x.x

--- interview/python/triple_access_codes.py
+++ interview/python/triple_access_codes.py
@@ -1,16 +1,19 @@
 def answer(l):
-    
+    count = 0
+    for i in range(1, len(l) - 1):
+        left = []
+        right = []
+        for j in range(0, i):
+            if l[i] % l[j] == 0:
+                left.append(l[j])
+        for j in range(i + 1, len(l)):
+            if l[j] % l[i] == 0:
+                right.append(l[j])
+        count += len(left) * len(right)
+
+    return count
+
 
 if __name__ == "__main__":
-    test = [1, 1, 1, 2, 2, 2, 3, 3, 3]
+    test = [5, 5, 5, 5, 5]
     print(answer(test))
-
-    """
-    1, 1, 1
-    1, 1, 2
-    1, 1, 3
-    1, 2, 2
-    1, 3, 3
-    2, 2, 2
-    3, 3, 3
-    """
