d4fa51af12e8b973462459e4a81193e91fea1a16
Derek <derekt1026@gmail.com>
2016-11-07 20:40:22 -0600

    Pinspired edit

--- Competitive Programming/pinterest/pinspired.py
+++ Competitive Programming/pinterest/pinspired.py
@@ -6,8 +6,10 @@ def pun():
         score = 0
         sentence = [elem for elem in input().split()]
         for word in sentence:
-            if len(word) >= 3 and word[:3].lower() == "pin" and word not in non_puns:
-                score += 1
+            for j in range(0, len(word) - 3):
+                if len(word) >= 3 and word[j:j + 3].lower() == "pin" and word not in non_puns:
+                    score += 1
+                    break
         print(score)
 
 if __name__ == "__main__":
