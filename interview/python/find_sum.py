abd7589b35682568f12426626740027fd5266a3b
Derek <derekt1026@gmail.com>
2016-12-16 22:25:59 -0600

    Find Sum linear solution

--- interview/python/find_sum.py
+++ interview/python/find_sum.py
@@ -1,17 +1,29 @@
 nums = [int(elem) for elem in input().split()]
 tgt_sum = int(input())
 
-front = 0
-back = len(nums) - 1
-curr_sum = nums[front] + nums[back]
-while front < back and curr_sum != tgt_sum:
-    curr_sum = nums[front] + nums[back]
-    if curr_sum < tgt_sum:
-        front += 1
-    elif curr_sum > tgt_sum:
-        back -= 1
+# Sorted solution
+# front, back = 0, len(nums) - 1
+# curr_sum = nums[front] + nums[back]
+# while front < back and curr_sum != tgt_sum:
+#     curr_sum = nums[front] + nums[back]
+#     if curr_sum < tgt_sum:
+#         front += 1
+#     elif curr_sum > tgt_sum:
+#         back -= 1
+#
+# if curr_sum == tgt_sum:
+#     print('Yes')
+# else:
+#     print('No')
 
-if curr_sum == tgt_sum:
-    print('Yes')
-else:
+# Non-sorted solution
+visited = set()
+found = False
+for i in nums:
+    if tgt_sum - i in visited:
+        print('Yes')
+        found = True
+        break
+    visited.add(i)
+if not found:
     print('No')
